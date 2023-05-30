55168f6fa0c10c05f6ed930c0b8f6bab26a606bb
PadgeM <padge.mattaliano@gmail.com>
2023-05-30 11:01:52 -0700

    adds boxes and X's on click

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  const handleGamePlay = (index) => {
+    // alert{index}
+    let updatedBoard = [...squares]
+    updatedBoard[index] = "X"
+    setSquares(updatedBoard)
+  }
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className='gameBoard'>
+      {squares.map((value, index) => {
+        return (
+          <Square
+          key={index}
+          value={value}
+          index={index}
+          handleGamePlay={handleGamePlay}
+          />
+        )
+      })}
+      </div>
     </>
   )
 }
+
 
 export default App
